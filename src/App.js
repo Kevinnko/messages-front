f6355e8f05ceb76b6e327bb3eeb66350222eda11
Kevin N <45763760+Kevinnko@users.noreply.github.com>
2019-05-03 12:54:15 +0200

    added form with controlled components, created handleChange and handleSubmit functions

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import "./App.css";
 class App extends React.Component {
   state = {
     messages: [],
-    loading: true
+    message: "",
+    status: "true"
   };
 
   async componentDidMount() {
@@ -16,18 +17,48 @@ class App extends React.Component {
     });
   }
 
+  handleChange = event => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = async event => {
+    event.preventDefault();
+
+    const response = await axios.post("http://localhost:3001/post-message", {
+      message: this.state.message,
+      public: this.state.status
+    });
+
+    this.setState({ messages: response.data });
+  };
+
   render() {
     const { messages } = this.state;
     return (
       <div className="container">
         {messages.map((message, index) => {
           return (
-            <div key={index}>
+            <div key={index} className="message">
               <div>{message.message}</div>
-              <div>{message.public ? <p>Public</p> : <p>Privé</p>}</div>
+              {message.public === "true" ? <div>Public</div> : <div>Privé</div>}
             </div>
           );
         })}
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="message" onChange={this.handleChange} />
+          <select
+            value={this.state.status}
+            name="status"
+            onChange={this.handleChange}
+          >
+            <option value="true">Public</option>
+            <option value="false">Privé</option>
+          </select>
+          <input type="submit" value="Envoyer" />
+        </form>
       </div>
     );
   }
