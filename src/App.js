8bbf47f8cdf1557dfb01a4dd828f01a31127747a
Kevin N <45763760+Kevinnko@users.noreply.github.com>
2019-05-06 16:35:07 +0200

    created stateless component for message list ; improved UI look and feel ; tried unit testing (not functional yet)

--- src/App.js
+++ src/App.js
@@ -10,11 +10,19 @@ class App extends React.Component {
   };
 
   async componentDidMount() {
+    var element = document.getElementsByClassName("scrollable-div")[0];
+    console.log("in componentDidMount", element);
+
     // loading messages from api
     const response = await axios.get("http://localhost:3001/get-messages");
     this.setState({
       messages: response.data
     });
+  }
+  componentDidUpdate() {
+    var element = document.getElementsByClassName("scrollable-div")[0];
+    console.log("in componentDidUpdate", element);
+    element.scrollIntoView(false);
   }
 
   handleChange = event => {
@@ -32,24 +40,39 @@ class App extends React.Component {
       public: this.state.status
     });
 
-    this.setState({ messages: response.data });
+    this.setState({
+      messages: response.data,
+      message: ""
+    });
   };
 
   render() {
     const { messages } = this.state;
+
     return (
       <div className="container">
-        {messages.map((message, index) => {
-          return (
-            <div key={index} className="message">
-              <div>{message.message}</div>
-              {message.public === "true" ? <div>Public</div> : <div>Privé</div>}
-            </div>
-          );
-        })}
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" name="message" onChange={this.handleChange} />
+        {/* S'il y a des messages, on les affiche, sinon on affiche "pas de message" */}
+        {messages.length === 0 ? (
+          <div>Pas de messages</div>
+        ) : (
+          <MessagesList messages={messages} />
+        )}
+        <form
+          className="form"
+          style={{ display: "flex" }}
+          onSubmit={this.handleSubmit}
+        >
+          <textarea
+            className="message-input"
+            placeholder="Ecrivez votre message..."
+            name="message"
+            cols="30"
+            rows="1"
+            value={this.state.message}
+            onChange={this.handleChange}
+          />
           <select
+            className="select"
             value={this.state.status}
             name="status"
             onChange={this.handleChange}
@@ -57,11 +80,37 @@ class App extends React.Component {
             <option value="true">Public</option>
             <option value="false">Privé</option>
           </select>
-          <input type="submit" value="Envoyer" />
+          <input
+            disabled={this.state.message === "" ? true : false}
+            className="button"
+            type="submit"
+            value="Envoyer"
+          />
         </form>
       </div>
     );
   }
 }
+// created stateless component
+export const MessagesList = props => {
+  return (
+    <div className="scrollable-div">
+      {props.messages.map((message, index) => {
+        return (
+          <>
+            <div key={index} className="message">
+              <div className="msg-text">{message.message}</div>
+              {message.public === "true" ? (
+                <div className="msg-status">Public</div>
+              ) : (
+                <div className="msg-status">Privé</div>
+              )}
+            </div>
+          </>
+        );
+      })}
+    </div>
+  );
+};
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import MessagesList from "./App";
+import axios from "axios";
+import { create } from "react-test-renderer";
+import { configure, mount, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+configure({ adapter: new Adapter() });
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+jest.mock("axios");
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe("<App />", () => {
+  it("should contain an array of objects with specific keys", async () => {
+    const response = {
+      data: [
+        { message: "Coucou", status: "true" },
+        { message: "Coucou", status: "false" }
+      ]
+    };
+    expect(Array.isArray(response.data)).toEqual(true);
+    axios.get.mockResolvedValue(response);
+    const component = create(<App />);
+    const instance = component.getInstance();
+    await instance.componentDidMount();
+
+    instance.state.messages.forEach(obj => {
+      expect(typeof obj).toEqual("object");
+
+      // chaque objet du tableau de messages doit contenir les clés 'message' et 'status
+      expect(Object.keys(obj).sort()).toEqual(["message", "status"]);
+
+      // Valider le type des valeurs de chaque clé
+      expect(typeof obj.message).toEqual("string");
+      expect(typeof obj.status).toEqual("string");
+    });
+  });
+
+  // tester le conditional public / privé
+  // tester le onSubmit
+});
+
+// le composant MessageList doit contenir une liste de messages
+describe("MessageList", () => {
+  it("should contain divs", () => {
+    const messages = [
+      { message: "Coucou", status: "true" },
+      { message: "Coucou", status: "false" }
+    ];
+
+    const component = mount(<MessagesList messages={messages} />);
+    const value = component.find("div.message");
+    console.log("component ", component);
+    console.log("value ", value);
+  });
 });
